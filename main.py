d9c36e6de7e6c79dae6f13c3adc087862078aafd
Göksu <111267914+goksucaglar@users.noreply.github.com>
2026-01-18 02:29:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # programı başlatan kod
+
+from map import Map # map.py dosyasına git, Map class’ını buraya getir.
+from robot import Robot
+
+def main():
+  world = Map(16, 16)
+  world.add_resource(1,1)
+  world.add_obstacle(2,0)
+  world.add_obstacle(3,0)
+
+robot = Robot(world, 0, 0, 100)
