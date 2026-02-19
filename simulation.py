b54dece13dc85d2ef1c6a502359cc7f7f6c43df0
Göksu <111267914+goksucaglar@users.noreply.github.com>
2026-02-19 23:50:32 +0300

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@
 def simulate(world, robot, steps = 20):
   step = 0
   while step < steps and robot.energy > 0 :
-    print(
+    print(f"Step {step} Pos: ({robot.x}{robot.y}) Energy: ({robot.energy}")
+    
     
   
