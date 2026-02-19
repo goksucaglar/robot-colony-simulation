178fda6389d160a1dc5f54961b758a050404e327
Göksu <111267914+goksucaglar@users.noreply.github.com>
2026-02-20 00:38:30 +0300

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -2,7 +2,7 @@
 def simulate(world, robot, steps = 20):
   step = 0
   while step < steps and robot.energy > 0 :
-    print(f"Step {step} Pos: ({robot.x}{robot.y}) Energy: ({robot.energy}")
+    print(f"Step {step} Pos: ({robot.x},{robot.y}) Energy: ({robot.energy}"))
 
     right = robot.look_cell("right")
     left = robot.look_cell("left")
