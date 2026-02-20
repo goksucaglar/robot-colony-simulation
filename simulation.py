0380156ef0fdc6351d1e78ffd8416f7c95ed61d8
Göksu <111267914+goksucaglar@users.noreply.github.com>
2026-02-20 03:05:09 +0300

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -24,9 +24,8 @@ def simulate(world, robot, steps = 20):
 
       if empty_directions:
         direction = random.choice(empty_directions)
-        print("Boş Yönler: ", empty_directions)
-        print("Seçilen Yön: ", direction)
-        print(f"Hareket denemesi → {direction}") # print("Hareket denemesi → " + direction)
+        print(f"Boş Yönler: {empty_directions} → Seçilen: {direction}")
+        # print(f"Hareket denemesi → {direction}") # print("Hareket denemesi → " + direction)
         robot.move(direction)
       else:
         print("Robot hareket edemez.")
