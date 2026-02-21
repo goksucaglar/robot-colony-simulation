89e589c38fa480efc007414833908ec783f33a40
Göksu <111267914+goksucaglar@users.noreply.github.com>
2026-02-22 01:39:54 +0300

    Update map.py

--- map.py
+++ map.py
@@ -20,3 +20,13 @@ class Map:
   def add_resource(self, x, y):
     if 0 <= y < self.height and 0 <= x < self.width:
       self.cells[y][x] = 2
+
+  def is_within_bounds(self, x, y):
+    return 0 <= x < self.width and 0 <= y < self.height
+    
+  def is_obstacle(self, x, y):
+    return self.cells[y][x] == 1
+
+  def is_resource(self, x, y):
+    return self.cells[y][x] == 2
+
